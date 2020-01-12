300358b5b292abb680c985bd35f54198f351be08
qing <2324233094@qq.com>
2020-01-12 16:52:07 +0800

    '实现输入/到home;输入/login到login'

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './components/home';
+import Login from './components/login';
 
 export default class App extends Component {
     render() {
-        return <Router>APP....</Router>
+        return <Router>
+            <Route path="/" exact component= {Home}/>
+            <Route path="/login" exact component= {Login}/>
+        </Router>
     }
 }
