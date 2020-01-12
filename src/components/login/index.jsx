2c3a8c4b0a6e37d778749f7b5a70e15a2a4202ad
qing <2324233094@qq.com>
2020-01-13 00:16:07 +0800

    'login表单校验规则二'

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -7,6 +7,37 @@ import './index.less';
 
 @Form.create()
 class Login extends Component {
+    //自定义校验规则
+    validator = (rule, value, callback) => {
+        /*
+          rule.field 获取表单key
+          value 获取表单value
+        */
+        // console.log(rule, value);
+
+        const name = rule.field === 'username' ? '用户名' : '密码';
+
+
+        const reg = /^\w+$/;
+
+        if (!value) {
+            //`${name}不能为空`  模板字符串
+            callback(`${name}不能为空`);
+        } else if (value.length < 4) {
+            callback(`${name}必须大于4位`);
+        } else if (value.length > 15) {
+            callback(`${name}必须小于15位`);
+        } else if (!reg.test(value)) {
+            callback(`${name}只能包含英文、数字、下划线`);
+        }
+        /*
+          callback() 调用不传参，代表表单校验成功
+          callback(message) 调用传参，代表表单校验失败，会提示message错误
+        */
+        // 必须要调用，否则会出问题
+        callback();
+    };
+
     render() {
 
         // getFieldDecorator 高阶组件：用来表单校验
@@ -29,7 +60,7 @@ class Login extends Component {
                                 {
                                     //required: true必填
                                     rules: [
-                                        {
+                                        /*{
                                             required: true,
                                             message: '用户名不能为空'
                                         },
@@ -44,6 +75,10 @@ class Login extends Component {
                                         {
                                             pattern: /^\w+$/,
                                             message: '用户名只能包含英文、数字、下划线'
+                                        }*/
+                                        {
+                                            //自定义表单校验规则,值为一个函数,为了复用可以定义在实例对象上
+                                            validator: this.validator
                                         }
                                     ],
                                 }
@@ -61,12 +96,27 @@ class Login extends Component {
                     </Form.Item>
                     <Form.Item>
 
-                        <Input
-                            prefix={
-                                <Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />
-                            }
-                            placeholder='密码'
-                        />
+                        {
+                            getFieldDecorator(
+                                //将来可以作为key,input为value
+                                'password',
+                                {
+                                    rules: [
+                                        {
+                                            //自定义表单校验规则,值为一个函数,为了复用可以定义在实例对象上
+                                            validator: this.validator
+                                        }
+                                    ]
+                                }
+                            )(
+                                <Input
+                                    prefix={
+                                        <Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />
+                                    }
+                                    placeholder='密码'
+                                />
+                            )
+                        }
 
                     </Form.Item>
                     <Form.Item>
